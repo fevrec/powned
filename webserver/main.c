b175fa46bf2a72fb28ec6593cbbb71f0b21e2a8e
Quentin PORION <quentin.porion@etudiant.univ-lille1.fr>
2016-02-05 10:16:07 +0100

    SIGCHLD

--- webserver/main.c
+++ webserver/main.c
@@ -5,26 +5,31 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <string.h>
 #include "socket.h"
 
 int main (void)
 {	
 	int socket_serveur = creer_serveur(8080);
-	
+	initialiser_signaux();	
+	int statut;
 	while (1) {
 		int socket_client = accept ( socket_serveur , NULL , NULL );
 		if ( socket_client == -1){
 			perror ( " accept " ); /* traitement d ’ erreur */
 		}
-		if (fork() == 0) {	
+		int pid;
+		if ((pid = fork()) == 0) {
+			
 			/* On peut maintenant dialoguer avec le client */
 			sleep(2);
 			const char * message_bienvenue = "Mesdames, messieurs,\n client, cliente\nJe suis heureux de vous annoncer que moi, serveur, s'est connecté à vous meme.\nPuisse le lien qui nous unis etre aussi fort que l'amour liant Mr java et Madame Eclipse.\nCe message sera envoyé tant que vous me recevrez, tant que vous m'ecouterez de votre oreille attentive...\nPaix et amour, guerre et haine, ne sont que futilitées, nous vivons notre passion comme un arbre perds ses feuilles en automne...\nVive les programmes ! Pro comme professionel, gramme comme le peu qu'il nous faut pour nous faire plaisir. Ajoutez a ces éléments, un peu de feuille marron et blanche afin d'obtenir un moment de paix et d'armonie autour de vous.\nA tout de suite pour un nouveau message !\nta\nmer\n" ;
 			write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
-			initialiser_signaux();
 			return 1;
 		}
+		if (waitpid(pid, &statut, WNOHANG) == -1)
+			perror("erreur waitpid");
 		close(socket_client);
 	}
 	return 0;
--- webserver/socket.c
+++ webserver/socket.c
@@ -25,8 +25,17 @@ int creer_serveur ( int port ) {
 	return socket_serveur;
 }
 
-void initialiser_signaux ( void ) {
-	if ( signal ( SIGPIPE , SIG_IGN ) == SIG_ERR ) {
-		perror ( " signal " );
+
+void traitement_signal(int sig) {
+	printf( "Signal %d reçu \n",sig);
+}
+
+void initialiser_signaux(void) {
+	struct sigaction sa ;
+	sa.sa_handler = traitement_signal ;
+	sigemptyset(&sa.sa_mask );
+	sa.sa_flags = SA_RESTART ;
+	if ( sigaction ( SIGCHLD , &sa , NULL ) == -1) {
+		perror ( "sigaction ( SIGCHLD )");
 	}
 }
