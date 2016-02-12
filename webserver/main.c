4a4c95b31e72cc86579de86673c40473e7f73992
Corwin Fevre <corwin.fevre@online.fr>
2016-02-12 11:01:54 +0100

    test

--- webserver/main.c
+++ webserver/main.c
@@ -24,7 +24,8 @@ int main (void)
 		if ((pid = fork()) == 0) {
 			/* On peut maintenant dialoguer avec le client */
 			//sleep(1);
-			/*const char * message_bienvenue = "Mesdames, messieurs,\n client, cliente\nJe suis heureux de vous annoncer que moi, serveur, s'est connecté à vous meme.\nPuisse le lien qui nous unis etre aussi fort que l'amour liant Mr java et Madame Eclipse.\nCe message sera envoyé tant que vous me recevrez, tant que vous m'ecouterez de votre oreille attentive...\nPaix et amour, guerre et haine, ne sont que futilitées, nous vivons notre passion comme un arbre perds ses feuilles en automne...\nVive les programmes ! Pro comme professionel, gramme comme le peu qu'il nous faut pour nous faire plaisir. Ajoutez a ces éléments, un peu de feuille marron et blanche afin d'obtenir un moment de paix et d'armonie autour de vous.\nA tout de suite pour un nouveau message !\n" ;
+			/*const char * message_bienvenue = "Mesdames, messieurs,\n client, cliente\nJe suis heur
+eux de vous annoncer que moi, serveur, s'est connecté à vous meme.\nPuisse le lien qui nous unis etre aussi fort que l'amour liant Mr java et Madame Eclipse.\nCe message sera envoyé tant que vous me recevrez, tant que vous m'ecouterez de votre oreille attentive...\nPaix et amour, guerre et haine, ne sont que futilitées, nous vivons notre passion comme un arbre perds ses feuilles en automne...\nVive les programmes ! Pro comme professionel, gramme comme le peu qu'il nous faut pour nous faire plaisir. Ajoutez a ces éléments, un peu de feuille marron et blanche afin d'obtenir un moment de paix et d'armonie autour de vous.\nA tout de suite pour un nouveau message !\n" ;
 			write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
 			*/
 	FILE *f = fdopen(socket_client, "r+");
@@ -36,6 +37,7 @@ int main (void)
 
 			return 1;
 		}
+        printf("connexion acceptée\n") ;
 		if (waitpid(pid, &statut, WNOHANG) == -1)
 			perror("erreur waitpid");
             close(socket_client);
